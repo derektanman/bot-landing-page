0a98afd9233719fc013c2ba3586ee141f94e7e08
Derek Tan <derektan@berkeley.edu>
2021-07-28 17:48:29 -0700

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test a
+          Henlo
         </p>
         <a
           className="App-link"
